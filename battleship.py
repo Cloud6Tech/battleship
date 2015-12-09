a69b7e45824dc9a166188941d41691641fc16801
hmccabe <hmccabe@csumb.edu>
2015-12-08 21:17:01 -0800

    Somehow lost previous commits, redoing

--- battleship.py
+++ battleship.py
@@ -3,6 +3,7 @@ from player import Player
 from ship import Ship
 from board import Board
 from utility import *
+from random import randint
 
 # Prompt the user for player type and create and return a human Player object or computer AI object with boards set up
 def createPlayer():
@@ -10,7 +11,8 @@ def createPlayer():
   listOfShips = [Ship(5,"aircraft carrier"),Ship(4,"battleship"),Ship(3,"submarine"),Ship(3,"cruiser"),Ship(2,"destroyer")]
 
   # Determine if the player is human (0) or a computer (1)
-  playerType = getOption("Player Type","Is this player a human or a computer?",["Human","Computer"])
+  #playerType = getOption("Player Type","Is this player a human or a computer?",["Human","Computer"])
+  playerType = 0
   
   if playerType == 0: # Human player
     # Get player's name
@@ -24,21 +26,64 @@ def createPlayer():
     player = Player(playerName)
     
     # Prompt player to set up their local board
-    player.setupLocalBoard(listOfShips)
+    #player.setupLocalBoard(listOfShips)
+    player.setupTestPlayer(listOfShips)
     
   #elif playerType == 1: # AI (computer) player
     
-  else: # Option pane was closed
-    return None
+  #else: # Option pane was closed
+  #  return None
     
   return player
 
 def battle():
-  
-  # Create the players
-  player1 = createPlayer()
-  player2 = createPlayer()
+
+  # Create the players, stored in a list for easier turn-taking
+  players = [createPlayer(), createPlayer()]
   
   # Exit if either player wasn't created
-  if player1 == None or player2 == None:
+  if players[0] == None or players[1] == None:
     return
+  
+  # Randomly pick a player to go first
+  n = randint(0,1)
+  
+  while True:
+    # Set current player and opponent
+    player = players[n]
+    opponent = players[abs(n-1)]
+  
+    # Get player's guess
+    guess = player.makeGuess()
+    
+    # Display the player's guess
+    printNow(player.getName() + " fired at " + str(guess) + ".")
+    
+    # Check if the player guessed correctly
+    strike = opponent.getLocalBoard().fireAt(guess)
+    
+    if strike == 0:  # The guess was wrong
+      # Update player's board
+      player.getLocalBoard().markMiss(guess)
+      
+      # Print message
+      printNow(player.getName() + " missed!")
+      
+    
+    else:            # The guess was correct
+      # Take 1 hit point away from the hit ship
+      strike.takeHit()
+      
+      # Update the player's board
+      player.getLocalBoard().markHit(guess)
+      
+      # Check if ship was sunk, display appropriate message
+      if strike.isSunk():  # The ship is sunk
+        printNow(player.getName() + " sunk " + opponent.getName() + "'s " + strike.getDescription() + "!")
+      else:                # The ship is hit, but not sunk
+        printNow(player.getName() + " hit " + opponent.getName() + "'s " + strike.getDescription() + "!")
+    
+    # Increment n to swap players next turn
+    n = abs(n-1)
+    
+    break  
