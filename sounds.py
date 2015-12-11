4c4a1ba5aa4fecc270c522f4c62ba1d9d43c59ed
bhansenotter <brhansen@csumb.edu>
2015-12-11 00:03:42 -0800

    Added Sound
    Uploading a file of all sound assets for program.
    made it easy to swap for local testing.
    
    Uploading sound class, very basic with just constructor, play, and stop
    functions.
    
    Instantiating sound objects in main .py file
    
    Will integrate into game once AI goes live.

---
+++ sounds.py
@@ -0,0 +1,18 @@
+# Battleship Game
+# CST 205
+# SoundEffect Class
+# Team: Jason Lloyd Heather Mccabe, Brett, Matthew Mason
+from media import *
+
+class SoundEffect:
+
+  def __init__(self, filePath):
+    self._sound = makeSound(filePath)
+    
+  def playStart(self):
+    play(self._sound)
+    return
+  
+  def playStop(self):
+    stopPlaying(self._sound)
+    return
