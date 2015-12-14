b5f3f3be21264fed1fdb0a3884e65064952f439a
hmccabe <hmccabe@csumb.edu>
2015-12-14 02:34:25 -0800

    redid comments, fixed 'A2' bug
    - Edited all comments and spacing to make the code easier to understand
    - Fixed 'A2' bug by initializing target to None and modifying following
    while loop

--- cpuPlayer.py
+++ cpuPlayer.py
@@ -1,7 +1,8 @@
-# Battleship Game
+# Final Project: Battleship Game
 # CST 205
-# CpuPlayer Class
-# Team: Jason Lloyd Heather Mccabe, Brett, Matthew Mason
+# Group Seven: Brett Hansen, Jason Lloyd, Matthew Mason, Heather McCabe
+# Last modified: Dec 14, 2015
+# CPUPlayer.py
 
 from media import *
 from ship import Ship
@@ -25,174 +26,207 @@ class CPUPlayer:
   def getLife(self):
     return len(self._listOfShips)
   
+  # Remove a ship from listOfShips (called when a ship is sunk)
   def removeShip(self, ship):
-    i = 0
-    for myShip in self._listOfShips:
-      if myShip == ship:
-        del self._listOfShips[i]
-        return true
-      else:
-        i += 1
-    return false
+    if ship in self._listOfShips:
+      self._listOfShips.remove(ship)
+      return true
+    else:
+      return false
         
-  # Use autoGuess Seek and Destroy algorithm to select a target to guess
+  # Use autoGuess() 'Seek and Destroy' algorithm to select a target to guess
   def makeGuess(self):
-    #calls autoGuess function makeGuess
     hitList = self._board.getHitList()
     guess = autoGuess(hitList, self._guesses)
     self._guesses.append(guess)
     return guess
       
+  # Automatically place ships on board
   def setupLocalBoard(self, listOfShips):
+    # Save list of ships
     self._listOfShips = listOfShips
 
+    # Set board axis ranges
     yAxis = "ABCDEFGHIJ"
     xMax = 10
     xMin = 1
     
-    #initialize coordinates
+    # Initialize coordinates
     yCoord = ''
     xCoord = 0
     runDirection = ['left', 'right', 'up', 'down']
-    # Automatically place ships
+    
+    # Place ships
     for i in range(0,len(listOfShips)):
       shipPlaced = false 
       while shipPlaced == false:
-        shipSpace = false
-        #check for space on board
-        while shipSpace == false:
-        #select random coordinates to place ships
+        
+        # Select random coordinates and directions until a set is found that allows space for the ship
+        shipHasSpace = false
+        while shipHasSpace == false:
           yCoord = random.choice(yAxis)
           xCoord = random.randint(xMin,xMax)
           direction = random.randint(0,len(runDirection)-1)
-          shipSpace = self._board.validateSpaceForShip(listOfShips[i], yCoord + str(xCoord),runDirection[direction])
-        #place ship on board
+          shipHasSpace = self._board.validateSpaceForShip(listOfShips[i], yCoord + str(xCoord),runDirection[direction])
+        
+        # Place ship on board
         shipPlaced = self._board.placeShip(listOfShips[i], yCoord + str(xCoord),runDirection[direction])
-    #repaint(self._board.getBoard())
+
     return
 
-############# Code for auto guess function #############
+############# 'Seek and Destroy': code for autoGuess() #############
+# Axis labels
 yAxis ="ABCDEFGHIJ"
+xMax = 10
+xMin = 1
+
+# Create list of every-other y-axis label
 yChoices = []
 for i in range(0,len(yAxis),2):
   yChoices.append(yAxis[i])
-xMax = 10
-xMin = 1
+
+# Level One: Called if hit list is empty; guesses every other square until a hit is made
+# Argument: axis switchFlag 
+# Returns target coordinate in string form
 def randomCoord(switchFlag):
-  # looking for flag to determine y axis is odd or x axis
-  if switchFlag == false: #skips xAxis
-    yCoord=random.choice(yAxis)
+  # switchFlag determines which axis will have guessed values skipped
+  if switchFlag == false: # Skip x-axis values
+    # Pick a random value on the y-axis
+    yCoord = random.choice(yAxis)
+    
+    # Pick a random odd value on the x-axis
     xCoord = random.choice(range(xMin,xMax,2))
-    if xCoord == 0:
-      xCoord = 1
+
+    # Create coordinate string
     target = yCoord + str(xCoord)
-  else: #skips yAxis
+  else: # Skip y-axis values
+    # Pick a random value from the list of every-other y-value
     yCoord = random.choice(yChoices)
+    
+    # Pick a random value on the x-axis
     xCoord = random.randint(xMin,xMax)
+    
+    # Create coordinate string
     target = yCoord + str(xCoord)
   
   return target
 
-# If a single hit has been made, randomLevel2() will guess around that point until another hit is made
-#searches around a hit area for another occupied location
-# takes in list current hit locations and used locations. 
-#Calls randomCoord as backup if no cases are matched
-#returns new target coordinate
+# Level Two: Called if exactly one hit has been made; guesses around that point until another hit is made
+# Arguments: list of current known hits, list of already-used coordinates, axis switchFlag
+# Returns target coordinate in string form
 def randomLevel2(hitCoord, usedCoord,switchFlag):
-  #pulls current hitCoord to separate y and x values
+  # Get current hitCoord and separate y and x values
    currentCoord  = hitCoord[0]
    yCoord = currentCoord[0]
    yIndex = yAxis.index(yCoord)
    xCoord = int(currentCoord[1])
-   #verifies x+1 will not go off the board and the new coord has not already been used
+   
+   # Verify x+1 is on the board and new target coord has not already been used
    if xCoord != xMax and (yCoord +str(xCoord+1) not in usedCoord):
+     # Target coordinate will be one space to the right of hit coordinate
      target = yCoord +str(xCoord+1)
      return target
-   #verifies y+1 will not go off the board and the new coord has not already been used
+   # Else verify y+1 is on the board and new target coord has not already been used
    elif yIndex != (len(yAxis)-1) and (yAxis[yIndex+1] +str(xCoord) not in usedCoord):
+     # Target coordinate will be one space below hit coordinate
      target = yAxis[yIndex+1] +str(xCoord)
      return target
-   #verifies x-1 will not go off the board and the new coord has not already been used
+   # Else verify x-1 is on the board and new target coord has not already been used
    elif xCoord != xMin and (yCoord +str(xCoord-1) not in usedCoord):
+     # Target coordinate will be one space to the left of hit coordinate
      target = yCoord +str(xCoord-1)
      return target
-   #verifies y-1 will not go off the board and the new coord has not already been used
+   # Else verify y-1 is on the board and new target coord has not already been used
    elif yIndex != 0 and (yAxis[yIndex-1] +str(xCoord) not in usedCoord):
+     # Target coordinate will be one space above hit coordinate
      target = yAxis[yIndex-1] +str(xCoord)
      return target
-   #used if no cases match
+   # Else if everything fails, go to randomCoord() logic
    else:
      return randomCoord(switchFlag)
 
-# Level three of search and destroy, called if at least two hits have been made; guesses along a common axis between the first and last coordinate in the list
+# Level Three: Called if at least two hits have been made; guesses along a common axis between the first and last coordinate in the list
 # Arguments: list of current known hits, list of already-used coordinates, axis switchFlag
 # Checks two hit points to verify if x or y is repeated then moves either + or - along the axis until the target ship is dead     
 def randomLevel3(hitCoord, usedCoord, switchFlag):
-  
-  # Pull out first hit coord from the hit list and separate x and y 
+  # Get first hitCoord and separate y and x values
   coordHit0  = hitCoord[0]
   yCoordHit0 = coordHit0[0]
   yIndexHit0 = yAxis.index(yCoordHit0)
   xCoordHit0 = int(coordHit0[1])
   
-  # Pull out last hit coord from the hit list and separate x and y
+  # Get last hitCoord and separate y and x values
   coordHit1  = hitCoord[len(hitCoord)-1]
   yCoordHit1 = coordHit1[0]
   yIndexHit1 = yAxis.index(yCoordHit1)
   xCoordHit1 = int(coordHit1[1])
   
-  # If y1 is larger than y0, guesses will move from y0 to y1 along xAxis
+  # If y0 < y1, guesses will move down from y1 or up from y0 (along the y-axis)
   if yIndexHit0 < yIndexHit1:
-    # Verify two points are on the same axis, yIndexHit1 hit coord is not on upper edge of board range, and new target coord has not been used
+    # Verify both points have same x-value, y1 hit coord is not on upper edge of board range, and new target coord has not been used
     if xCoordHit0 == xCoordHit1 and yIndexHit1 != (len(yAxis)-1) and (yAxis[yIndexHit1+1] + str(xCoordHit0) not in usedCoord):
-      # New target will be one space to the right of the higher hit coordinate
+      # New target will be one space below the y1 hit
       target = yAxis[yIndexHit1+1] +str(xCoordHit0)
       return target
-    # Otherwise, verify two points are on the same axis, yIndexHit0 hit coord is not on lower edge of board range, and new target coord has not been used
+      
+    # Else, verify both points have same x-value, y0 hit coord is not on lower edge of board range, and new target coord has not been used
     elif xCoordHit0 == xCoordHit1 and yIndexHit0 != (0) and (yAxis[yIndexHit0-1] +str(xCoordHit0) not in usedCoord):
-      # New target will be one space above the smaller hit coordinate
+      # New target will be one space above the y0 hit
       target = yAxis[yIndexHit0-1] + str(xCoordHit0)
       return target
   
-  # If y0 is larger than y1, guesses will move in opposite direction along xAxis. 
+  # If y0 > y1, guesses will move down from y0 or up from y1 (along the y-axis)
   elif yIndexHit0 > yIndexHit1:
-    # verifies to point are on the same axis and checks to ensure new target coord will stay on the board and has not been used. 
+    # Verify both points have same x-value, y0 is not on upper edge of board range, and new target coord has not been used
     if xCoordHit0 == xCoordHit1 and yIndexHit0 != (len(yAxis)-1) and (yAxis[yIndexHit0+1] +str(xCoordHit0) not in usedCoord):
-       target = yAxis[yIndexHit0+1] +str(xCoordHit0)
-       return target
-    # verifies to point are on the same axis and checks to ensure new target coord will stay on the board and has not been used.
+      # New target will be one space below the y0 hit
+      target = yAxis[yIndexHit0+1] +str(xCoordHit0)
+      return target
+       
+    # Else, verify both points have same x-value, y1 is not on the lower edge of board range, and new target coord has not been used
     elif xCoordHit0 == xCoordHit1 and yIndexHit1 != (0) and (yAxis[yIndexHit1-1] +str(xCoordHit0) not in usedCoord):
-       target = yAxis[yIndexHit1-1] +str(xCoordHit0)
-       return target
-  #checks to see if y0 is smaller than y1. if true the will move in smallest to larget along xAxis
+      # New target will be one space above the y1 hit
+      target = yAxis[yIndexHit1-1] +str(xCoordHit0)
+      return target
+       
+  # If x0 < x1, guesses will move right from x1 or left from x0 (along the x-axis)
   elif xCoordHit0 < xCoordHit1:
-    # verifies to point are on the same axis and checks to ensure new target coord will stay on the board and has not been used.
+    # Verify both points have same y-value, x1 is not on the upper edge of board range, and new target coord has not been used
     if yCoordHit0 == yCoordHit1 and xCoordHit1 != xMax and (yCoordHit0 +str(xCoordHit1+1) not in usedCoord):
-       target = yCoordHit0 +str(xCoordHit1+1)
-       return target
-    # verifies to point are on the same axis and checks to ensure new target coord will stay on the board and has not been used.
+      # New target will be one space to the right of x1 hit
+      target = yCoordHit0 +str(xCoordHit1+1)
+      return target
+       
+    # Else, verify both points have same y-value, x0 is not on the lower edge of board range, and new target coord has not been used
     elif yCoordHit0 == yCoordHit1 and xCoordHit0 != xMin and (yCoordHit0 +str(xCoordHit0-1) not in usedCoord):
-       target = yCoordHit0 +str(xCoordHit0-1)
-       return target
-  #if x0 is larger than x1. If true then the algorithm will grow in opposite direction along y axis.
+      # New target will be one space to the left of x0 hit
+      target = yCoordHit0 +str(xCoordHit0-1)
+      return target
+       
+  # If x0 > x1, guesses will move right from x0 or left from x1 (along the x-axis)
   elif xCoordHit0 > xCoordHit1:
-    # verifies to point are on the same axis and checks to ensure new target coord will stay on the board and has not been used.
+    # Verify both points have same y-value, x0 is not on the upper edge of board range, and new target coord has not been used
     if yCoordHit0 == yCoordHit1 and xCoordHit0 != xMax and (yCoordHit0 +str(xCoordHit0+1) not in usedCoord):
-       target = yCoordHit0 +str(xCoordHit0+1)
-       return target
-    # verifies to point are on the same axis and checks to ensure new target coord will stay on the board and has not been used.
+      # New target will be one space to the right of x0 hit
+      target = yCoordHit0 +str(xCoordHit0+1)
+      return target
+       
+    # Else, verify both points have same y-value, x1 is not on the lower edge of board range, and new target coord has not been used
     elif yCoordHit0 == yCoordHit1 and xCoordHit1 != xMin and (yCoordHit0 +str(xCoordHit1-1) not in usedCoord):
-       target = yCoordHit0 +str(xCoordHit1-1)
-       return target
+      # New target will be one space to the left of x1 hit
+      target = yCoordHit0 +str(xCoordHit1-1)
+      return target
+      
+  # Else if everything fails, go to randomLevel2() logic
   else:
     return randomLevel2(hitCoord, usedCoord,switchFlag)
   
-# Main AI control. 
+# Main AI guessing control. 
 # Arguments: list of current known hits, list of already-used coordinates
 # Returns coordinate in string form, i.e. "A1"
 def autoGuess(hitCoord, usedCoord):
-  # Set intial target postion
-  target = "A2"
+  # Intialize target variable
+  target = None
   
   # Set starting x position for switchFlag check
   lastX = 1
@@ -212,7 +246,8 @@ def autoGuess(hitCoord, usedCoord):
   # If hit list is empty then AI will select every other square until a hit is made
   if len(hitCoord) < 1:
     # Pick a random target that has not been guessed
-    while target in usedCoord:
+    print target
+    while target == None or target in usedCoord:
       target = randomCoord(switchFlag)
     return target
   
@@ -228,7 +263,7 @@ def autoGuess(hitCoord, usedCoord):
     # Pick a target on the common axis that has not been guessed
     while target in usedCoord:
       target = randomLevel3(hitCoord, usedCoord, switchFlag)
-      # Final catch for null string caused by adjacent vertical ships; go back to randomLevel2() logic
+      # Final catch for null string caused by adjacent vertical ships; go to randomLevel2() logic
       if target == None:
         target = randomLevel2(hitCoord, usedCoord, switchFlag)
     return target
