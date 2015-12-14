94159630f679f12e74111357e1b2ec6edc83f605
hmccabe <hmccabe@csumb.edu>
2015-12-14 00:04:03 -0800

    cleaning up
    - Updated comments
    - Removed some old commented-out code
    - Rearranged some code for consistency in battle()
    - Rearranged some code for more responsive-seeming play (board is
    updated and message is printed before the sound starts rather than after
    it ends)
    - Instead of checking that player name is 'CPU', now checks if the
    player object is of the Player class using isinstance()
    - Removed some old stuff in createPlayer() from when I was going to make
    it an option

--- battleship.py
+++ battleship.py
@@ -1,30 +1,30 @@
+# Final Project: Battleship Game
+# Group Seven: Brett Hansen, Jason Lloyd, Matthew Mason, Heather McCabe
+# Last modified: Dec 13, 2015
+# battleship.py
 
-showInformation('Please select the folder containing the library files')
-libPath = pickAFolder() 
-#setLibPath("D:\\Heather\\Documents\\School\\CSIT\\2015 Fall B - CST 205\\battleship")
-#setLibPath('C:\\Users\\Bretterbear\\Documents\\GitHub\\battleship')
+# Set the directory for imports and sound files
+showInformation('Please select the folder containing the library files.')
+libPath = pickAFolder()
 setLibPath(libPath)
 soundDirectory = libPath + '\\SoundFiles\\'
 
+# Import modules/libraries
 from player import Player
+from cpuPlayer import CPUPlayer
 from ship import Ship
 from board import Board
 from sounds import SoundEffect
-#from utility import *
 from random import randint
 from time import sleep
-from cpuPlayer import CPUPlayer
 
-# Prompt the user for player type and create and return a human Player object or computer AI object with boards set up
-# Prompt the user for player type and create and return a human Player object or computer AI object with boards set up
-def createPlayer(type):
+# Create and return a human Player object (type = 0) or computer AI object (type = 1) with boards set up
+def createPlayer(playerType):
   # Create list of ships to place on boards
+  # Format: Ship(length,type)
   listOfShips = [Ship(5,"aircraft carrier"),Ship(4,"battleship"),Ship(3,"submarine"),Ship(3,"cruiser"),Ship(2,"destroyer")]
 
-  # Determine if the player is human (0) or a computer (1)
-  #playerType = getOption("Player Type","Is this player a human or a computer?",["Human","Computer"])
-  playerType = type
-  
+  # Set player name and create player object using appropriate class for type
   if playerType == 0: # Human player
     # Get player's name
     playerName = requestString("What is your name?")
@@ -35,29 +35,26 @@ def createPlayer(type):
       
     # Create player object  
     player = Player(playerName)
-    
-    # Prompt player to set up their local board
-    player.setupLocalBoard(listOfShips)
-    #player.setupTestPlayer(listOfShips)
-    
+       
   elif playerType == 1: # AI (computer) player
+    # Set player's name
     playerName = 'CPU'
-    player = CPUPlayer(playerName)
     
-    # Prompt player to set up their local board
-    player.setupLocalBoard(listOfShips)
+    # Create CPUPlayer object
+    player = CPUPlayer(playerName)
+       
+  # Prompt human Player to set up board or automatically set up CPUPlayer Board
+  player.setupLocalBoard(listOfShips)
+      
   # Update the title of the game board Picture object with the player's name
   player.getBoard().getBoard().setTitle(playerName + "'s Fleet")
     
   return player
 
+# Play a game of Battleship
 def battle():
 
   # Create the soundEffects to be used in the program
-  #Change this to reflect your sound directory in order to make audio work
-  #soundDirectory = 'C:\\Users\\Bretterbear\\Documents\\GitHub\\battleship\\SoundFiles'
-  #soundDirectory = 'C:\\Users\\masonm\\CST205\\battleship\\SoundFiles'
-  
   sndDeploy = SoundEffect(soundDirectory + '\\deployEffect.wav')
   sndDefeat = SoundEffect(soundDirectory + '\\defeatEffect.wav')
   sndFire = SoundEffect(soundDirectory + '\\fireEffect.wav')
@@ -67,7 +64,8 @@ def battle():
   sndVictory = SoundEffect(soundDirectory + '\\victoryEffect.wav')
   
   # Create the players, stored in a list for easier turn-taking
-  players = [createPlayer(1), createPlayer(0)]
+  # player[0] is human, player[1] is AI/computer
+  players = [createPlayer(0), createPlayer(1)]
   
   # Exit if either player wasn't created
   if players[0] == None or players[1] == None:
@@ -80,14 +78,15 @@ def battle():
     # Set current player and opponent
     player = players[n]
     opponent = players[abs(n-1)]
-    board = players[n].getBoard()
-    if player.getName() != 'CPU':
-      repaint(board.getBoard())
+    
+    # Show board if current player is human
+    if isinstance(player,Player):
+      repaint(players[n].getBoard().getBoard())
   
-    # Get player's guess; guessed coordinate will be validated
+    # Get player's guess (guessed coordinate will be validated)
     guess = player.makeGuess()
     
-    # If makeGuess returned None, player clicked cancel; exit
+    # Player clicked cancel; exit game
     if guess == None:
       return
     
@@ -97,54 +96,66 @@ def battle():
     sleep(1)
     
     # Check if the player guessed correctly
-
     ship = opponent.getBoard().fireAt(guess)
     
     if ship == 0:  # The guess was wrong
       # Update player's board
       player.getBoard().markMiss(guess, 'upper')
-      if player.getName() != 'CPU':
-        repaint(board.getBoard())
       
       # Update opponent's board
       opponent.getBoard().markMiss(guess, 'lower')
       
+      # Update display of human player's board
+      if isinstance(player,Player):
+        repaint(player.getBoard().getBoard())
+      elif isinstance(opponent,Player):
+        repaint(opponent.getBoard().getBoard())
+            
       # Print message and play audio
       printNow(player.getName() + " missed!")
       sndMiss.playStart()
       sleep(1)
     
-    else:
-      # The guess was correct
+    else: # The guess was correct
       # Update the player's board
       player.getBoard().markHit(guess, 'upper')
-      if player.getName() != 'CPU':
-        repaint(board.getBoard())
-      # Take 1 hit point away from the hit ship
-      ship.takeHit()
-      sndHit.playStart()
-      sleep(1)
-            
+      
       # Update the opponent's board
       opponent.getBoard().markHit(guess, 'lower')
       
-      # Check if ship was sunk, display appropriate message
-      if ship.isSunk():  # The ship is sunk
+      # Update display of human player's board
+      if isinstance(player,Player):
+        repaint(player.getBoard().getBoard())
+      elif isinstance(opponent,Player):
+        repaint(opponent.getBoard().getBoard())
+        
+      # Take 1 hit point away from the hit ship
+      ship.takeHit()
+      
+      # Print message and play sound
+      printNow(player.getName() + " hit " + opponent.getName() + "'s " + ship.getDescription() + "!")
+      sndHit.playStart()
+      sleep(1)
+            
+      # Check if ship was sunk
+      if ship.isSunk():
+        # Print message and play audio
         printNow(player.getName() + " sunk " + opponent.getName() + "'s " + ship.getDescription() + "!")
         sndSink.playStart()
         sleep(1)
+        
         # Remove ship from opponent's list of ships
         opponent.removeShip(ship)
-        #clears hit list in players board - primary use is for CPU makeGuess
+        
+        # Clear hit list in player's board - used for CPU makeGuess()
         player.getBoard().clearHitList()
-        # Check if opponent has lost, print message and exit if so
+        
+        # Check if opponent has lost; print message, play sound, and exit if so
         if opponent.getLife() == 0:
           printNow(player.getName() + " wins!")
           sndVictory.playStart()
           sleep(1)
           return
-      else:                # The ship is hit, but not sunk
-        printNow(player.getName() + " hit " + opponent.getName() + "'s " + ship.getDescription() + "!")
     
     # Increment n to swap players next turn
     n = abs(n-1)
